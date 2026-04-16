3fda00dd70ca2b6da5eb0a981d2c71f98eae4a5d
nmhmihi <bapnho2000@gmail.com>
2026-04-16 08:04:08 +0800

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+export default nextConfig;
